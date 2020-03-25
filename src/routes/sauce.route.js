28643bec19a177bdf208eae261df23658c2f217a
Camille Briand <camille.briand@efrei.net>
2020-03-25 23:47:13 +0100

    Improve /sauce subenpoints

--- src/routes/sauce.route.js
+++ src/routes/sauce.route.js
@@ -5,6 +5,20 @@ const ow = require("ow");
 const S3Service = require("../services/s3-service");
 
 /**
+ * Describes the route endpoints
+ * @param {*} req Incoming request
+ * @param {*} res Outgoing response
+ */
+function describe (req, res) {
+	return res.status(404).json({
+		description: "To add a new quote sauce: POST /sauce/quote\nTo add a new image sauce: POST /sauce/image"
+	});
+}
+
+router.get("/", describe);
+router.post("/", describe);
+
+/**
  * POST /sauce/quote
  * 
  * Request parameters:
@@ -24,6 +38,10 @@ router.post("/quote", async (req, res) => {
 		});
 	}
 
+	return res.status(500).json({
+		message: "This endpoint has not been implemented yet"
+	});
+
 	// TODO: store quote and answer in database record with service
 });
 
@@ -42,19 +60,27 @@ router.post("/image", async (req, res) => {
 		});
 	}
 
-	try {
-		const s3Response = await S3Service.uploadFileToS3(req.files.image);
-		return res.status(200).json({
-			message: 'File uploaded',
-			url: s3Response.fileUrl
-		});
+	let s3Response;
 
-		// TODO: store image url and answer in database record with service
+	// Attempt to store the image in the S3 Bucket
+	try {
+		s3Response = await S3Service.uploadFileToS3(req.files.image);
 	} catch (err) {
 		return res.status(400).json({
 			message: 'Unable to upload file'
 		});
 	}
+
+	// TODO: store image url and answer in database record with service
+
+	return res.status(500).json({
+		message: "This endpoint has not been implemented yet"
+	});
+
+	return res.status(201).json({
+		message: "Sauce created",
+		url: s3Response.fileUrl
+	});
 });
 
 module.exports = router;
