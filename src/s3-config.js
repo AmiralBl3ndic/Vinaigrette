69868e01845b58d7c0b1c382833c9eff42c8206b
Camille Briand <camille.briand@efrei.net>
2020-03-25 18:49:15 +0100

    Start to work on s3-config.js

--- src/s3-config.js
+++ src/s3-config.js
@@ -1,3 +1,35 @@
+const aws = require("aws-sdk");
 
+aws.config.update({
+	region: process.env.AWS_REGION_CODE,
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
 
-module.exports = {};
+const s3 = new aws.S3();
+
+const s3BucketName = process.env.AWS_S3_BUCKET_NAME;
+
+function uploadFileToS3(file) {
+	return new Promise(async (resolve, reject) => {
+		// TODO: type checking for [file] 
+
+		const uploadData = {
+			Bucket: s3BucketName,
+			Key: file.name,
+			Body: file.data
+		};
+
+		s3.upload(uploadData, (err, s3Data) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve({
+					fileUrl: s3Data
+				});
+			}
+		});
+	});
+}
+
+module.exports = uploadFileToS3;
