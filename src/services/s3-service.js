1713cf4349ea8573b13eeee60c833b1e6d398a27
Camille Briand <camille.briand@efrei.net>
2020-03-25 19:19:50 +0100

    Rename s3-config.js to s3-service.js

---
+++ src/services/s3-service.js
@@ -0,0 +1,37 @@
+const aws = require("aws-sdk");
+
+aws.config.update({
+	region: process.env.AWS_REGION_CODE,
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const s3 = new aws.S3();
+
+const s3BucketName = process.env.AWS_S3_BUCKET_NAME;
+
+class S3Service {
+	static uploadFileToS3(file) {
+		return new Promise(async (resolve, reject) => {
+			// TODO: type checking for [file] 
+
+			const uploadData = {
+				Bucket: s3BucketName,
+				Key: Date.now() + file.name,
+				Body: file.data
+			};
+
+			s3.upload(uploadData, (err, s3Data) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve({
+						fileUrl: s3Data.Location
+					});
+				}
+			});
+		});
+	}
+};
+
+module.exports = S3Service;
