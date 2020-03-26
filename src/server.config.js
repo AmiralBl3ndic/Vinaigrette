e9d6d36ffbbb8ec9b491110320b70ec6a94ea3f2
Camille Briand <camille.briand@efrei.net>
2020-03-26 19:14:01 +0100

    Add a default rate limit by IP in src/server.config.js

--- src/server.config.js
+++ src/server.config.js
@@ -1,3 +1,16 @@
+const rateLimit = require("express-rate-limit");
+
+/**
+ * The maximum number of requests a single IP will be able to make to
+ * endpoints protected by the rate limiter 
+ */
+const maximumAPIRequestsByHourAndByIp = 60;
+
 module.exports = {
-	mongoConnectionString: process.env.CONTAINERIZED === "true" ? "mongodb://mongo:27017/" : "mongodb://localhost:27017/"
+	mongoConnectionString: process.env.CONTAINERIZED === "true" ? "mongodb://mongo:27017/" : "mongodb://localhost:27017/",
+
+	rateLimiter: rateLimit({
+		windowMs: 1000 * 60 * 60,  // 1 hours time window
+		max: maximumAPIRequestsByHourAndByIp
+	})
 };
