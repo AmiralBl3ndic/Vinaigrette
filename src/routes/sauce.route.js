b8d28ffa2b5d2fd02258990321d832498dd95171
Camille Briand <camille.briand@efrei.net>
2020-03-26 18:12:27 +0100

    Update /sauce endpoints documentation

--- src/routes/sauce.route.js
+++ src/routes/sauce.route.js
@@ -16,15 +16,36 @@ function describe (req, res) {
 	});
 }
 
+/**
+ * ? GET /sauce
+ * 
+ * Describes the sub endpoints of the /sauce route
+ */
 router.get("/", describe);
+
+/**
+ * ? POST /sauce
+ * 
+ * Describes the sub endpoints of the /sauce route
+ */
 router.post("/", describe);
 
 /**
- * POST /sauce/quote
+ * ? GET /sauce/random
  * 
- * Request parameters:
- * quote {string}: Quote to save
- * answer {string}: Answer for this sauce
+ * Returns a random `QuoteSauce` or `ImageSauce` with specified type
+ */
+router.get("/random", async (req, res) => {
+	return res.status(500).json({ message: "Endpoint not implemented yet" });
+});
+
+/**
+ * ? POST /sauce/quote
+ * 
+ * Registers a new `QuoteSauce` and saves it to the database
+ * 
+ * @param {String} req.body.quote Quote to save
+ * @param {String} req.body.answer Answer for this sauce
  */
 router.post("/quote", async (req, res) => {
 	if (req.body.quote === undefined || req.body.quote === "") {  // If no quote or empty quote
@@ -59,11 +80,15 @@ router.post("/quote", async (req, res) => {
 });
 
 /**
- * POST /sauce/image
+ * ? POST /sauce/image
  * 
- * Request parameters:
- * image {file}: File to save
- * answer {string}: Awaited answer
+ * Registers a new `ImageSauce` and saves it to the database
+ * 
+ * @param {Object} req.files.image Image to save
+ * @param {String} req.files.image.mimetype MIME type of the uploaded file (should be "image/jpeg" or "image/png")
+ * @param {String} req.files.image.name Name of the image
+ * @param {Buffer} req.files.image.data Non-empty buffer of bytes representing the image
+ * @param {String} req.body.answer Answer for this sauce
  */
 router.post("/image", async (req, res) => {
 	if (req.files === undefined || req.files.image === undefined) {  // Image type request, no images passed in
