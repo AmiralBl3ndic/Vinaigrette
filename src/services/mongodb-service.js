fc769eebe4e03e23d63fad4b43c4f222c586f59c
Camille Briand <camille.briand@efrei.net>
2020-03-26 15:53:14 +0100

    Add MongoDBService.getQuoteSauceFromRequest method

---
+++ src/services/mongodb-service.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const errorCodes = require("../error-codes");
+
+const QuoteSauce = require("../models/quote-sauce");
+const ImageSauce = require("../models/image-sauce");
+
+/**
+ * Wrapper around MongoDB-related actions
+ */
+class MongoDBService {
+	/**
+	 * Get a `QuoteSauce` from an incoming `express.Request`
+	 * @param {express.Request} Incoming request
+	 * @throws An error when `req.body`, `req.body.quote` or `req.body.answer` have falsy value 
+	 */
+	static getQuoteSauceFromRequest(req) {
+		if (!req.body || !req.body.quote || !req.body.answer) {
+			throw new Error({
+				errorCode: errorCodes.E_BAD_ARGUMENT,
+				message: "Request is missing required data"
+			});
+		}
+
+		// Convert answer to a usable format
+		let { answer } = req.body;
+		answer = answer.replace(/ /g, "").toLowerCase();
+
+		return new QuoteSauce({
+			answer,
+			_id: new mongoose.Types.ObjectId(),
+			quote: req.body.quote
+		});
+	}
+};
+
+module.exports = MongoDBService;
