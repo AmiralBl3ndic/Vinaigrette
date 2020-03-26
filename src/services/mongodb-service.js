4d6b3c2cb4ec39d5b9e552612077f685dc2043f7
Camille Briand <camille.briand@efrei.net>
2020-03-26 16:48:31 +0100

    Add MongoDBService.getImageSauceFromRequest method

--- src/services/mongodb-service.js
+++ src/services/mongodb-service.js
@@ -4,6 +4,8 @@ const { formatAnswer } = require("../utils");
 
 const errorCodes = require("../error-codes");
 
+const S3Service = require("./s3-service");
+
 const QuoteSauce = require("../models/quote-sauce");
 const ImageSauce = require("../models/image-sauce");
 
@@ -13,10 +15,10 @@ const ImageSauce = require("../models/image-sauce");
 class MongoDBService {
 	/**
 	 * Get a `QuoteSauce` from an incoming `express.Request`
-	 * @param {express.Request} Incoming request
+	 * @param {express.Request} req Incoming request
 	 * @throws An error when `req.body`, `req.body.quote` or `req.body.answer` have falsy value 
 	 */
-	static getQuoteSauceFromRequest(req) {
+	static getQuoteSauceFromRequest (req) {
 		if (!req.body || !req.body.quote || !req.body.answer) {
 			throw new Error({
 				errorCode: errorCodes.E_BAD_ARGUMENT,
@@ -31,6 +33,34 @@ class MongoDBService {
 			answer,
 			_id: new mongoose.Types.ObjectId(),
 			quote: req.body.quote
+		});
+	}
+
+	/**
+	 * Get an `ImageSauce` from an incoming `express.Request`
+	 * 
+	 * This function attempts to store the image contained in the request into the configured S3 bucket,
+	 * but will throw an error if unable to get a HTTP link to the image.
+	 * @param {express.Request} req Incoming
+	 */
+	static async getImageSauceFromRequest (req) {
+		if (!req.body || !req.body.answer || !req.files || !req.files.image) {
+			throw new Error({
+				errorCode: errorCodes.E_BAD_ARGUMENT,
+				message: "Request is missing required data"
+			});
+		}
+
+		// Upload image to S3 bucket and gather the public URL of the image
+		const { fileUrl } = await S3Service.uploadImage(req.files.image);
+
+		// Convert answer to a usable format
+		const answer = formatAnswer(req.body.answer);
+
+		return new ImageSauce({
+			answer,
+			_id: new mongoose.Types.ObjectId(),
+			imageUrl: fileUrl
 		});
 	}
 };
