a1becfceb303f5691edbd228c17fe3946440e42d
Camille Briand <camille.briand@efrei.net>
2020-03-26 14:49:42 +0100

    Add base schema for quote sauce

---
+++ src/models/quote-sauce.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const quoteSauceSchema = mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	quote: String,
+	answer: String
+});
+
+module.exports = mongoose.model('QuoteSauce', quoteSauceSchema, 'sauces');
