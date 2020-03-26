1d87470ffd3cd2bd0b2088929a0ce53cdbe0556e
Camille Briand <camille.briand@efrei.net>
2020-03-26 15:22:41 +0100

    Add base schema for image sauce

---
+++ src/models/image-sauce.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const imageSauceSchema = mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	imageUrl: String,
+	answer: String
+});
+
+module.exports = mongoose.model('ImageSauce', imageSauceSchema, 'sauces');
