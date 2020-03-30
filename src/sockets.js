cb75f15abd79f768bf1755756df07cc8b1be4a04
Camille Briand <camille.briand@efrei.net>
2020-03-30 14:48:33 +0200

    Improve SocketHandler

--- src/sockets.js
+++ src/sockets.js
@@ -7,11 +7,24 @@ class SocketHandler {
 	 * @param {SocketIO.Socket} socket Client socket
 	 */
 	constructor (socket) {
+		console.info(`Client connected (${socket.id})`);
+
 		this.socket = socket;
+		this.emit = this.socket.emit;  // Shorthand
+		this.on = this.socket.on;  // Shorthand
+		this.join = this.socket.join;  // Shorthand
 
 		this.score = undefined;
 
 		// Bind socket events to SocketHandler actions
+		this.on('disconnect', this.handleDisconnect);
+	}
+
+	/**
+	 * Handes disconnection of a client
+	 */
+	handleDisconnect () {
+		console.info(`Client disconnected (${this.socket.id})`);
 	}
 }
 
