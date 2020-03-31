5414d51d3282c3625c07c96e84b97ebe478d099a
Camille Briand <camille.briand@efrei.net>
2020-03-31 11:07:56 +0200

    Implement Room.start function to start games

--- src/models/room.js
+++ src/models/room.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-param-reassign */
+
+const { formatAnswer } = require('../utils');
+const MongoDBService = require('../services/mongodb-service');
+const ImageSauce = require('./image-sauce');
+
 class Room {
 	/**
 	 * SocketIO Server
@@ -57,6 +63,13 @@ class Room {
 		 */
 		this.playersSockets = [];
 
+		/**
+		 * Points that an answer will give for the round
+		 * 
+		 * @type {Number}
+		 */
+		this.roundPoints = 5;
+
 		// Add room to list of rooms
 		Room.rooms.push(this);
 	}
@@ -67,6 +80,105 @@ class Room {
 	 */
 	hasAnyPlayerWon (points) {
 		return this.playersSockets.some((player) => player.score >= points);
+	}
+
+	/**
+	 * Start a game in the room
+	 */
+	async start () {
+		const pointsToWin = 100;
+		const roundDuration = 25 * 1000;  // 25 seconds
+		const timeBetweenRounds = 4 * 1000;  // 4 seconds
+
+		// Ensure all players have score set to 0
+		this.playersSockets = this.playersSockets.map((socket) => {
+			socket.score = 0;
+			return socket;
+		});
+
+		const startGameRound = async () => {
+			// Get a random sauce
+			const sauce = await MongoDBService.getRandomSauce();
+
+			// If no sauce found
+			if (!sauce) {
+				Room.io.in(this.name).emit('no_sauces_available');
+				Room.io.in(this.name).emit('game_end');
+				return;  // Nothing to be done
+			}
+
+			// Determine type of sauce and send it to players
+			if (sauce instanceof ImageSauce) {
+				Room.io.in(this.name).emit('new_round_sauce', {
+					type: 'image',
+					imageUrl: sauce.imageUrl,
+				});
+			} else {
+				Room.io.in(this.name).emit('new_round_sauce', {
+					type: 'quote',
+					quote: sauce.quote,
+				});
+			}
+
+			// Listen for players answers
+			this.playersSockets.forEach((socket) => socket.on('sauce_answer', (answer) => {
+				if (formatAnswer(answer).equals(sauce.answer)) {
+					// Increase player score
+					socket.points += this.roundPoints;
+					
+					// Decrease points won by next player (until it gets under 3 points)
+					if (this.roundPoints >= 3) {
+						this.roundPoints -= 2;
+					}
+
+					// Notify player of good answer
+					socket.emit('good_answer');
+
+					// Notify all players of scoreboard update
+					Room.io.in(this.name).emit('scoreboard_update', {
+						player: socket.username,
+						found: true,
+						score: socket.score,
+					});
+				} else {
+					// Notify player of wrong answer
+					socket.emit('wrong_answer');
+				}
+			}));
+
+			// Wait for round duration before doing anything
+			setTimeout(() => {
+				// Stop listening to player answers
+				this.playersSockets.forEach((socket) => socket.removeListener('sauce_answer'));
+
+				// Notify players of round end
+				Room.io.in(this.name).emit('round_end');
+
+				// If no player has won yet, start a new round after a timeout
+				if (!this.hasAnyPlayerWon(pointsToWin)) {
+					// Send good answer to all players
+					Room.io.in(this.name).emit('right_answer', {
+						answer: sauce.originalAnswer,
+					});
+
+					setTimeout(() => {
+						startGameRound();
+					}, timeBetweenRounds);
+				} else {  // If a player has won
+					// Determine winner (highest score)
+					// TODO: determine winner with first player to reach goal
+					const winningPlayers = this.playersSockets
+						.map((player) => ({ username: player.username, score: player.score }))  // Get only relevant data
+						.filter((player) => player.score >= pointsToWin)  // Get only players with 'winning' score
+						.sort((p1, p2) => p2.score - p1.score);  // Sort winning players by score in descending order
+
+					// Notify all players of winner
+					Room.io.in(this.name).emit('player_won', winningPlayers[0]);
+				}
+			}, roundDuration);  // TODO: find a way to shorten duration during round
+		};
+
+		startGameRound();  // Start the first game round
 	}
 }
 
